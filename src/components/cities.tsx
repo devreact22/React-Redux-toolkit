0cd3bf6fc05afb39b3690de819fe37d2bfdbcc4d
Dev <117921993+devreact22@users.noreply.github.com>
2024-03-10 19:15:40 +0100

    fix map

--- src/components/cities.tsx
+++ src/components/cities.tsx
@@ -3,40 +3,45 @@ import { RootState } from "../redux/store";
 import { fetchCities } from "../redux/cities/citiesSlice";
 import { useEffect, useState } from "react";
 
+interface Cities {
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
+
 const City: React.FC = () => {
-  const [cit, setCit] = useState([]);
+  const [cities, setCities] = useState<Cities[]>([]);
   const dispatch = useDispatch();
-  const cities = useSelector((state: RootState) => state.cities);
 
   useEffect(() => {
-    dispatch(fetchCities()), setCit(cit);
+    dispatch(fetchCities()) // Dispatch fetch action directly
+      .then((data: any) => setCities(data.payload)) // Update state with fetched data
+      .catch((error: any) => console.error("Error fetching cities:", error));
   }, []);
 
-  console.log("Cities");
-  console.log(cities);
+  console.log("Cities", cities);
 
   return (
     <>
-      <div className="grid grid-cols-4 gap-2">
-        { cit.map((items: any) => (
-            <div className="w-full rounded-lg shadow-md lg:max-w-sm" >
-              <img
-                className="object-cover w-full h-48"
-                src={items.thumbnailUrl}
-                alt="image"
-              />
-              <div className="p-4">
-                <h4 className="text-xl font-semibold text-blue-600">
-                  {items.title}
-                </h4>
-                {/* <p className="mb-2 leading-normal">
-                            {items.content}
-                            </p> */}
-                <button className="px-4 py-2 text-sm text-blue-100 bg-blue-500 rounded shadow">
-                  Read more
-                </button>
-              </div>
+      <div className="grid grid-cols-4 gap-4 text-black m-4">
+        {cities.map((city: Cities, key) => (
+          <div className="w-full rounded-lg shadow-md lg:max-w-sm" key={key}>
+            <img
+              className="object-cover w-full h-48"
+              src={city.thumbnailUrl}
+              alt="image"
+            />
+            <div className="p-4">
+              <h4 className="text-xl font-semibold text-blue-600 ">
+                {city.title}
+              </h4>
+              <p className="mb-2 leading-normal">{city.title}</p>
+              <button className="px-4 py-2 text-sm text-blue-100 bg-blue-500 rounded shadow">
+                Read more
+              </button>
             </div>
+          </div>
         ))}
       </div>
     </>
